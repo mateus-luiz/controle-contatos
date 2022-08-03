233527831af1c4181d7e3b1f33ebcdc8c7d3312c
Mateus <mateus.florentino2001@gmail.com>
2022-08-03 16:49:28 -0300

    Create all pages and edit front end using bootstrap

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
